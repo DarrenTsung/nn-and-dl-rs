46df9c4a584f7e0aed5995031ef2076c4f299cff
Darren Tsung <darren@onesignal.com>
2019-09-15 16:10:37 -0700

    Refactor CostLike to CostFunction trait

--- neurons/src/cost.rs
+++ neurons/src/cost.rs
@@ -1,13 +1,21 @@
-pub trait CostLike {
-    fn cost_against_expected(&self, expected: &[f64]) -> f64;
+pub struct Expected<'a>(&'a [f64]);
+pub struct Output<'a>(&'a [f64]);
+
+pub trait CostFunction {
+    fn cost(&self, output: Output, expected: Expected) -> f64;
 }
 
-impl CostLike for Vec<f64> {
-    fn cost_against_expected(&self, expected: &[f64]) -> f64 {
-        assert_eq!(self.len(), expected.len());
+pub struct Pow2CostFunction;
+
+impl CostFunction for Pow2CostFunction {
+    fn cost(&self, output: Output, expected: Expected) -> f64 {
+        let output = output.0;
+        let expected = expected.0;
+
+        assert_eq!(output.len(), expected.len());
 
         let mut cost = 0.0;
-        for (index, val) in self.iter().enumerate() {
+        for (index, val) in output.iter().enumerate() {
             cost += (val - expected[index]).powi(2);
         }
         cost
@@ -24,7 +32,7 @@ mod tests {
         let output = vec![0.0, 0.5, 1.0];
         let expected = vec![1.0, 1.0, 1.0];
 
-        let cost = output.cost_against_expected(&expected);
+        let cost = Pow2CostFunction.cost(Output(&output), Expected(&expected));
         assert_abs_diff_eq!(cost, 1.25);
     }
 }
